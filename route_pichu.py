13c2a7d20f9252f3808edd05f02f86d811b7be6c
Pradeep Reddy Rokkam <prokkam@iu.edu>
2021-09-12 01:59:49 -0400

    Implemented BFS(made use of visted nodes)

--- route_pichu.py
+++ route_pichu.py
@@ -24,6 +24,17 @@ def moves(map, row, col):
         # Return only moves that are within the house_map and legal (i.e. go through open space ".")
         return [ move for move in moves if valid_index(move, len(map), len(map[0])) and (map[move[0]][move[1]] in ".@" ) ]
 
+# Find the direction of the move from the two given moves(current move and future move)
+def direction(current_move,future_move):
+        if current_move[0]<future_move[0]:
+                return "D"
+        elif current_move[0]>future_move[0]:
+                return "U"
+        elif current_move[1]<future_move[1]:
+                return "R"
+        else:
+                return "L"
+
 # Perform search on the map
 #
 # This function MUST take a single parameter as input -- the house map --
@@ -36,23 +47,47 @@ def moves(map, row, col):
 def search(house_map):
         # Find pichu start position
         pichu_loc=[(row_i,col_i) for col_i in range(len(house_map[0])) for row_i in range(len(house_map)) if house_map[row_i][col_i]=="p"][0]
-        fringe=[(pichu_loc,0)]
+        fringe=[(pichu_loc,0,"")] 
+        print(fringe) #dummy
+
+        visited_nodes=[pichu_loc]
+
+        i=100
 
         while fringe:
-                (curr_move, curr_dist)=fringe.pop()
+                if i<0:
+                        break 
+                print(i)
+                print("\n")
+                (curr_move, curr_dist, dirctn)=fringe.pop()
+                # print("Current move is {}".format(curr_move))
+                # print("For the current move {} the following are the possible moves".format(curr_move))
                 for move in moves(house_map, *curr_move):
+                        # print(move)
+                        i=i-1
                         if house_map[move[0]][move[1]]=="@":
-                                return (7, 'DDDDDDD')  # return a dummy answer
+                                print("Goal Reached {}".format(curr_dist+1))
+                                d= direction(curr_move,move)
+                                return (curr_dist+1, dirctn+d)  # return a dummy answer
                         else:
-                                fringe.append((move, curr_dist + 1))
+                                if move not in visited_nodes:
+                                        d= direction(curr_move,move)
+                                        fringe.insert(0,(move, curr_dist + 1,dirctn+d))
+                                        visited_nodes.append(move)
+                print(fringe)
+        return (-1, '')  # return a path length of -1 as there is no solution from agent me
+                
+
+        
 
 # Main Function
 if __name__ == "__main__":
         house_map=parse_map(sys.argv[1])
+        # print(house_map)
         print("Shhhh... quiet while I navigate!")
         solution = search(house_map)
         print("Here's the solution I found:")
         print(str(solution[0]) + " " + solution[1])
-
         print("Test Check")
+
 
